2938e22e39ae13623e7493e30cd8f4dc9d8564bf
PK O'Flaherty <93098788+pkofy@users.noreply.github.com>
2022-02-13 16:46:17 -0500

    Add files via upload

---
+++ DATA607WK2Assignment.sql
@@ -0,0 +1,15 @@
+CREATE TABLE movieratings (
+    id int IDENTITY PRIMARY KEY,
+    "Free Guy" INT,
+    "Shang-Chi" INT,
+    "Chaos Walking" INT,
+    "Cruella" INT,
+    "Nomadland" INT,
+    "Parasite" INT
+);
+
+INSERT INTO movieratings VALUES (3, 4, 4, 4, 3, 5);
+INSERT INTO movieratings VALUES (5, 5, 5, 3, 3, 2);
+INSERT INTO movieratings VALUES (4, 5, 2, 4, 5, 5);
+INSERT INTO movieratings VALUES (2, 3, 4, 2, 1, 4);
+INSERT INTO movieratings VALUES (5, 5, 4, 5, 4, 5);
